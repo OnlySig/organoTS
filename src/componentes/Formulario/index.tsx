c5ee4beb72e76f9da942da3b9367cd6e1397afee
OnlySig <gabriel.castro.pereira11@gmail.com>
2024-02-01 18:09:29 -0300

    criando interface IColaborador.ts + tipando funcoes + formulario e listaSuspensa para tsx

--- src/componentes/Formulario/index.tsx
+++ src/componentes/Formulario/index.tsx
@@ -3,17 +3,23 @@ import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
+import { IColaborador } from '../../shared/interfaces/Icolaborador'
 
-const Formulario = (props) => {
+interface FormularioProps {
+    aoColaboradorCadastrado: (colaborador: IColaborador) => void,
+    times: string[]
+}
+
+const Formulario = ({ aoColaboradorCadastrado, times } :FormularioProps) => {
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
     const [time, setTime] = useState('')
 
-    const aoSalvar = (evento) => {
+    const aoSalvar = (evento: React.FormEvent<HTMLFormElement>) => {
         evento.preventDefault()
-        props.aoColaboradorCadastrado({
+        aoColaboradorCadastrado({
             nome,
             cargo,
             imagem,
@@ -30,29 +36,30 @@ const Formulario = (props) => {
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto 
-                    obrigatorio={true}
+                    obrigatorio
                     label="Nome"
                     placeholder="Digite seu nome" 
                     valor={nome}
                     aoAlterado={valor => setNome(valor)}
                 />
                 <CampoTexto
-                    obrigatorio={true}
+                    obrigatorio
                     label="Cargo"
                     placeholder="Digite seu cargo" 
                     valor={cargo}
                     aoAlterado={valor => setCargo(valor)}
                 />
                 <CampoTexto
+                    obrigatorio
                     label="Imagem"
                     placeholder="Digite o endereço da imagem" 
                     valor={imagem}
                     aoAlterado={valor => setImagem(valor)}
                 />
                 <ListaSuspensa
-                    obrigatorio={true}
+                    obrigatorio
                     label="Time" 
-                    itens={props.times}
+                    itens={times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
                 />
--- src/shared/interfaces/IColaborador.ts
+++ src/shared/interfaces/IColaborador.ts
@@ -1,5 +1,8 @@
 export interface IColaborador {
     nome: string,
     cargo: string,
-    imagem: string
+    imagem: string,
+    time?: string
 }
+
+// IColaborador é .ts e nâo .tsx pois IColaborador nâo é um componente para usar o jsx.
