142f73ed199cd0563dd52c9f40ba7a8a53a84956
OnlySig <gabriel.castro.pereira11@gmail.com>
2024-02-01 16:10:38 -0300

    add: tsconfig com npx tsc --init + interface dos componentes + tipagens dos elementos dos componentes

---
+++ src/componentes/Botao/index.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from 'react'
+import './Botao.css'
+
+interface BotaoProps {
+    children: ReactElement
+}
+
+const Botao = ({ children }: BotaoProps) => {
+    return (<button className='botao'>
+        {children}
+    </button>)
+}
+
+export default Botao
